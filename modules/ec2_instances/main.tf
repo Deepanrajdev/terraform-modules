588d95efc8ab46b74384992607a3b2e4880c912a
Deepanraj <deepanrajc311@gmail.com>
2025-05-27 14:22:21 +0530

    Update main.tf

--- modules/ec2_instances/main.tf
+++ modules/ec2_instances/main.tf
@@ -3,7 +3,7 @@
 }
 
 module "ec2_instance" {
-    source = "./modules/ec2_instance"
+    source = "./modules/ec2_instances"
     ami_value = "ami-026c39f4021df9abe"
     instance_type = "t2.micro"
 }
