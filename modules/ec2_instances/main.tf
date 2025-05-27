f53ab0a4919f6a40ca0647cad9525a228943310d
Deepanraj <deepanrajc311@gmail.com>
2025-05-27 13:13:14 +0530

    first commit

---
+++ modules/ec2_instances/main.tf
@@ -0,0 +1,9 @@
+ provider "aws" {
+    region = "ap=northeast-1"
+}
+
+module "ec2_instance" {
+    source = "./modules/ec2_instance"
+    ami_value = "ami-026c39f4021df9abe"
+    instance_type = "t2.micro"
+}
