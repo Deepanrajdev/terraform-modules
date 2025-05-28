aeaa44680b62e8cf1a64007ceecaba6e0e1a96dd
Deepanraj <deepanrajc311@gmail.com>
2025-05-28 10:58:13 +0530

    Update main.tf

--- modules/ec2_instances/main.tf
+++ modules/ec2_instances/main.tf
@@ -1,9 +1,9 @@
  provider "aws" {
     region = "ap=northeast-1"
 }
+ 
 
-module "ec2_instance" {
-    source = "./modules/ec2_instances"
-    ami_value = "ami-026c39f4021df9abe"
-    instance_type = "t2.micro"
-}
+ resource "aws_instance" "ec2_creation" {
+        ami = var.ami_value
+        instance_type = var.instance_type
+ }
